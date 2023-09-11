9b3ac0808dde49e61757c3ff11fd1ec8dee31f64
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-11 19:12:58 +0300

    change Program cs to create web service,

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp;
 using WebApp.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(opts =>
@@ -9,7 +10,40 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.EnableSensitiveDataLogging(true); 
 });
 var app = builder.Build();
-app.UseMiddleware<TestMiddleware>();
+const string BASEURL = "api/products";
+app.MapGet($"{BASEURL}/{{id}}",async (HttpContext context,DataContext data) => 
+{
+    string? id = context.Request.RouteValues["id"] as string;
+    if(id!= null)
+    {
+        Product? p =data.Products.Find(long.Parse(id));
+        if(p==null)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+        else
+        {
+            context.Response.ContentType="application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(p));
+        }
+    }
+});
+app.MapGet(BASEURL,async (HttpContext context,DataContext data) =>
+{
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
+});
+app.MapPost(BASEURL,async (HttpContext context,DataContext data) => 
+{
+    Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+    if(p!=null)
+    {
+        await data.AddAsync(p);
+        await data.SaveChangesAsync();
+        context.Response.StatusCode = StatusCodes.Status200OK;
+    }
+});
+//app.UseMiddleware<TestMiddleware>();
 app.MapGet("/", () => "Hello World!");
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
