e5b1adc8aa6ce9f12d49bdcf994588c30d899f13
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-12 19:40:49 +0300

    change Products cs to use attribute ApiController

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,8 @@
 using WebApp.Models;
 
 namespace WebApp.Controllers;
+
+[ApiController]
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
@@ -21,24 +23,20 @@ public class ProductsController : ControllerBase
         Product? p = await context.Products.FindAsync(id);
         if (p == null)
         {
-            return NotFound(); 
+            return NotFound();
         }
         return Ok(p);
     }
     [HttpPost]
-    public async Task<IActionResult> SaveProduct([FromBody] ProductBindingTarget target)
+    public async Task<IActionResult> SaveProduct(ProductBindingTarget target)
     {
-        if(ModelState.IsValid)
-        {
-            Product p = target.ToProduct();
-            await context.Products.AddAsync(p);
-            await context.SaveChangesAsync();
-            return Ok(p);
-        }
-        return BadRequest();
+        Product p = target.ToProduct();
+        await context.Products.AddAsync(p);
+        await context.SaveChangesAsync();
+        return Ok(p);
     }
     [HttpPut]
-    public async Task UpdateProduct([FromBody] Product product)
+    public async Task UpdateProduct(Product product)
     {
         context.Products.Update(product);
         await context.SaveChangesAsync();
@@ -52,6 +50,6 @@ public class ProductsController : ControllerBase
     [HttpGet("redirect")]
     public IActionResult Redirect()
     {
-        return RedirectToAction(nameof(GetProduct),new { Id = 1});
+        return RedirectToAction(nameof(GetProduct), new { Id = 1 });
     }
 }
