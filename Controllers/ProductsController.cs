ffb234df421c6111a0ab21d1c513f2ec79ee8866
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-12 19:12:33 +0300

    change ProductsController cs to use redirect to action

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -48,6 +48,6 @@ public class ProductsController : ControllerBase
     [HttpGet("redirect")]
     public IActionResult Redirect()
     {
-        return Redirect("/api/products/1");
+        return RedirectToAction(nameof(GetProduct),new { Id = 1});
     }
 }
