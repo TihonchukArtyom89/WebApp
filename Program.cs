5f514b9315344c74dae150c3abe4590c0b70e55a
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-14 17:56:05 +0300

    change Program cs to configure JSON Serializer for all project to omit null properties

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(opts =>
@@ -9,6 +10,10 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.EnableSensitiveDataLogging(true); 
 });
 builder.Services.AddControllers();
+builder.Services.Configure<JsonOptions>(opts => 
+{
+    opts.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 var app = builder.Build();
 app.MapControllers();
 //app.UseMiddleware<TestMiddleware>();
