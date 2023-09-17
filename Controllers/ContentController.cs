2d300a800a46833c63810b48181893a128d5bb06
TihonchukArtyom89 <mortus1989@gmail.com>
2023-09-17 18:24:27 +0300

    add ContentController cs file to Controllers folder

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ContentController : ControllerBase
+{
+    private DataContext context;
+    public ContentController(DataContext dataContext)
+    {
+        context = dataContext;
+    }
+    [HttpGet("string")]
+    public string GetString() => "This is string response.";
+    [HttpGet("object")]
+    public async Task<Product> GetObject()
+    {
+        return await context.Products.FirstAsync();
+    }
+}
