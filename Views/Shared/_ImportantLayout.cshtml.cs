f870f22d81ffff01ccbdd8000077bff8aa30b7a6
TihonchukArtyom89 <mortus1989@gmail.com>
2024-01-01 12:52:30 +0300

    add new layout _ImportantLayout cshtml in Views/Shared,change Index cshtml in Views/Home to use specific layout

---
+++ Views/Shared/_ImportantLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Views.Shared
+{
+    public class _ImportantLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
